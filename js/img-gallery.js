a7988bec2b333a087727b591afe68f854a40c292
thegias <giasone@mac.com>
2013-10-21 22:19:48 +0100

    tyding up

--- js/img-gallery.js
+++ js/img-gallery.js
@@ -1,7 +1,8 @@
 /*define all variables*/
 var photoPathArray = new Array();
-var clickedPhotoIndex
-var firstGalleryPhoto
+var clickedPhotoIndex;
+var firstGalleryPhoto;
+var htmlGallery;
 
 /*populate array with all photo on the page*/
 function populatePhotoArray() {
@@ -9,9 +10,9 @@ function populatePhotoArray() {
 		photoPathArray[i] = $(this).attr('src');
 		console.log(photoPathArray[i]);
 	});
-}
+};
 
-/*store the index of the clicked image*/
+/*store the path of the clicked image*/
 function getClickedPhotoIndex() {
 	$('#portfolio a img').on('click', function(event) {
 		event.preventDefault();
@@ -22,44 +23,31 @@ function getClickedPhotoIndex() {
 	});
 };
 
-
+/*open gallery view, in the same tab, showing clicked photo*/
 function openGalleryView() {
 	$('#portfolio a img').on('click', function(event) {
 		event.preventDefault();
 		var w = window.open('gallery.html','_self',false);
 		console.log('got to openGalleryPage ' + firstGalleryPhoto);
-		var htmlFirstGalleryPhoto = '<img src="' + firstGalleryPhoto + '"/>';
-		$(w.document.body).html(htmlFirstGalleryPhoto).addClass('gallery');
+		var htmlGallery = '<img src="' + firstGalleryPhoto + '"/>';
+		$(w.document.body).html(htmlGallery);
+		alert('pause');
 		console.log('holding photo???');
-		
 	});
 };
 
+/*handles navigation in the gallery view*/
 /*function updateGalleryPhoto() {
+	on mouse-click/right-arrow-key/right-arrow-button 'next'
+	on delete/left-arrow-key/left-arrow-button 'previous'
+	on esc revert to last image shown
 };*/
 
+/*calling all functions in the right order*/
 $(document).ready( function() {
 	populatePhotoArray();
 	getClickedPhotoIndex();
 	openGalleryView();
+/*	updateGalleryPhoto;*/
 });
 
-
-/*
-call functions in order:
- 1)store path photo clicked
- 2)open page
- 3)forward/backward arrows
- 4)return to original page
-*/
-
-/*
-copy all photo paths on the page to an array
-upon click on any photo
-	copy image path
-	find the key value in the array
-	open up on gallery page full size (better with animation, blurr rest of screen)
-		on mouse-click/right-arrow-key/right-arrow-button 'next'
-		on delete/left-arrow-key/left-arrow-button 'previous'
-		on esc revert to last image shown
-*/	
